e090c4feada59acc8390581868ca20b477ed1c6b
dmitry_khodakovsky@epam.com <dmitry_khodakovsky@epam.com>
2019-03-25 22:09:44 +0300

    added comments for HW1 #1

--- src/test/java/homework1/ParameterizedTests.java
+++ src/test/java/homework1/ParameterizedTests.java
@@ -4,15 +4,17 @@ import org.testng.annotations.Test;
 import org.testng.annotations.Parameters;
 import static org.testng.Assert.*;
 
-
+// TODO Java naming convention for methods name
 public class ParameterizedTests extends TestBase {
 
+    // TODO для чего здесь используется параметризация, если папаметров нету??
     @Test(groups = {"Parameterized"})
     @Parameters(value = "positive")
     public void SignPositiveSimpleTest() {
         assertTrue(calculator.isPositive(8));
     }
 
+    // TODO для чего здесь используется параметризация, если папаметров нету??
     @Test(groups = {"Parameterized"})
     @Parameters(value = "positive")
     public void SignPositiveTestWithNegative() {
@@ -25,12 +27,14 @@ public class ParameterizedTests extends TestBase {
         assertTrue(calculator.isPositive(positive));
     }
 
+    // TODO для чего здесь используется параметризация, если папаметров нету??
     @Test(groups = {"Parameterized"})
     @Parameters(value= "negative")
     public void SignNegativeSimpleTest() {
         assertTrue(calculator.isNegative(-98));
     }
 
+    // TODO для чего здесь используется параметризация, если папаметров нету??
     @Test(groups = {"Parameterized"})
     @Parameters(value= "negative")
     public void SignSimpleTest() {

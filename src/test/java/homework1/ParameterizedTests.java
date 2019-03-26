7d9ae5be6b3198d1d6877c55d26177077ab5b7f4
Renata Kalimullina <krr12.04.98@mail.ru>
2019-03-26 21:20:55 +0300

    edited

--- src/test/java/homework1/ParameterizedTests.java
+++ src/test/java/homework1/ParameterizedTests.java
@@ -2,48 +2,29 @@ package homework1;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.Parameters;
+
 import static org.testng.Assert.*;
 
-// TODO Java naming convention for methods name
+// TODO Java naming convention for methods name - Исправлено
 public class ParameterizedTests extends TestBase {
 
-    // TODO для чего здесь используется параметризация, если папаметров нету??
     @Test(groups = {"Parameterized"})
     @Parameters(value = "positive")
-    public void SignPositiveSimpleTest() {
-        assertTrue(calculator.isPositive(8));
-    }
-
-    // TODO для чего здесь используется параметризация, если папаметров нету??
-    @Test(groups = {"Parameterized"})
-    @Parameters(value = "positive")
-    public void SignPositiveTestWithNegative() {
-        assertFalse(calculator.isPositive(-186));
-    }
-
-    @Test(groups = {"Parameterized"})
-    @Parameters(value = "positive")
-    public void SignPositiveTest(long positive) {
+    public void positiveSignTest(long positive) {
         assertTrue(calculator.isPositive(positive));
     }
 
-    // TODO для чего здесь используется параметризация, если папаметров нету??
-    @Test(groups = {"Parameterized"})
-    @Parameters(value= "negative")
-    public void SignNegativeSimpleTest() {
-        assertTrue(calculator.isNegative(-98));
-    }
 
-    // TODO для чего здесь используется параметризация, если папаметров нету??
+    // TODO для чего здесь используется параметризация, если папаметров нету?? - Исправлено
     @Test(groups = {"Parameterized"})
-    @Parameters(value= "negative")
-    public void SignSimpleTest() {
-        assertFalse(calculator.isNegative(186));
+    @Parameters(value = "positive")
+    public void notPositiveSignTest(long positive) {
+        assertFalse(calculator.isNegative(positive));
     }
 
     @Test(groups = {"Parameterized"})
-    @Parameters(value= "negative")
-    public void SignNegativeTest(long negative) {
+    @Parameters(value = "negative")
+    public void negativeSignTest(long negative) {
         assertTrue(calculator.isNegative(negative));
     }
 
